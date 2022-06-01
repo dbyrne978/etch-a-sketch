d7c029380e593017e6c59863758f035e81e9d6d6
Daniel Byrne <dbyrne978@gmail.com>
2022-05-31 22:34:17 -0400

    Seperate js code into named functions

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,30 @@
-// get number of rows/columns
-let rows = 100;
-let columns = 100;
-let numOfBoxes = rows * columns;
-
-// create boxes
-const container = document.querySelector("#container");
-for (let i = 0; i < numOfBoxes; i++) {
-    let box = document.createElement("div");
-    box.classList.add("box");
-    box.style.flexGrow = "1";
-    box.style.flexShrink = "1";
-    box.style.flexBasis = `calc(100% / ${columns})`;
-    box.classList.add(`column-${i % columns + 1}`);
-    box.classList.add(`row-${Math.floor(i / columns) + 1}`);
-    container.appendChild(box);
+let createBoxes = (gridSize) => {
+    const container = document.querySelector("#container");
+    let numOfBoxes = gridSize * gridSize
+    for (let i = 0; i < numOfBoxes; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.flexGrow = "1";
+        box.style.flexShrink = "1";
+        box.style.flexBasis = `calc(100% / ${gridSize})`;
+        box.classList.add(`column-${i % gridSize + 1}`);
+        box.classList.add(`row-${Math.floor(i / gridSize) + 1}`);
+        container.appendChild(box);
+    };
 };
 
-let boxList = document.querySelectorAll(".box");
-boxList.forEach((box) => {
-    box.addEventListener("mouseenter", e => {
-        e.target.style.backgroundColor = "black";
+let addSketchability = () => {
+    let boxList = document.querySelectorAll(".box");
+    boxList.forEach((box) => {
+        box.addEventListener("mouseenter", e => {
+            e.target.style.backgroundColor = "black";
+        });
     });
-});
+};
+
+let gridSetup = (gridSize) => {
+    createBoxes(gridSize);
+    addSketchability();
+};
+
+gridSetup(16);
